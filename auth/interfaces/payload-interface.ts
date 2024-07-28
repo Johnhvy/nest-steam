0f649307630aac5a632d46971b6219c18329ea24
johnhvy <pro.dev0530@outlook.com>
2024-07-28 15:18:11 +0900

    add user schema and type

---
+++ auth/interfaces/payload-interface.ts
@@ -0,0 +1,5 @@
+/* eslint-disable prettier/prettier */
+export interface JwtPayload {
+    userId: string;
+  }
+  
---
+++ auth/interfaces/register-status-interface.ts
@@ -0,0 +1,6 @@
+/* eslint-disable prettier/prettier */
+export interface RegistrationStatus {
+    success: boolean;
+    message: string;
+  }
+  
